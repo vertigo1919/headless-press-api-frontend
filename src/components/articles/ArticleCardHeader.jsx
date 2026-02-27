34b1a07a676e1779ebcf09c210c2878c9ca517dd
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-02-27 15:06:44 +0100

    add more button to article card header

--- src/components/articles/ArticleCardHeader.jsx
+++ src/components/articles/ArticleCardHeader.jsx
@@ -1,5 +1,8 @@
 import { formatDistanceToNowStrict } from 'date-fns';
 
+// I still need to add share functionality to more button
+// will need to calculate article URL id
+
 export function ArticleCardHeader({ article }) {
   // I may need to import as a prop viewType laterdown the line
   const timeStamp = formatDistanceToNowStrict(article.created_at)
@@ -20,6 +23,13 @@ export function ArticleCardHeader({ article }) {
         </div>
         <span className="article-card-header-topic-name">p/{article.topic}</span>
         <span className="article-card-header-time-stamp">{timeStamp}</span>
+        <button className="article-card-header-more-btn" type="button">
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
+            <circle cx="10" cy="5" r="2" />
+            <circle cx="10" cy="10" r="2" />
+            <circle cx="10" cy="15" r="2" />
+          </svg>
+        </button>
       </div>
       <h2>{article.title}</h2>
     </header>
