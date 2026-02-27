b2d5bdf11ed28a1c1b490032dea7b321e9173edc
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-02-27 14:55:44 +0100

    add actual time difference timestap to article-header as well as css

--- src/components/articles/ArticleCardHeader.jsx
+++ src/components/articles/ArticleCardHeader.jsx
@@ -1,17 +1,27 @@
+import { formatDistanceToNowStrict } from 'date-fns';
+
 export function ArticleCardHeader({ article }) {
   // I may need to import as a prop viewType laterdown the line
-  const timeStamp = article.created_at;
+  const timeStamp = formatDistanceToNowStrict(article.created_at)
+    .replace(' minutes', 'm')
+    .replace(' hours', 'h')
+    .replace(' days', 'd')
+    .replace(' years', 'y');
 
   return (
-    <header className="article-card-header-main">
-      <img
-        className="article-card-header-topic-image"
-        src={article.topic_img_url}
-        alt={article.topic}
-      ></img>
-      <span className="article-card-header-topic-name">p/{article.topic}</span>
-      <span className="article-card-header-time-stamp">p/{timeStamp}</span>
-      <div>{article.title}</div>
+    <header className="article-card-header">
+      <div className="article-card-header-meta">
+        <div className="article-card-header-topic-image-container">
+          <img
+            className="article-card-header-topic-image"
+            src={article.topic_img_url}
+            alt={article.topic}
+          ></img>
+        </div>
+        <span className="article-card-header-topic-name">p/{article.topic}</span>
+        <span className="article-card-header-time-stamp">{timeStamp}</span>
+      </div>
+      <h2>{article.title}</h2>
     </header>
   );
 }
