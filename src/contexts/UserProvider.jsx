630c882a2e39138cff690d04c9d2905744508092
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-03-03 22:16:36 +0000

    feat: change harcoded user from string to object

--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -7,7 +7,7 @@ import { UserContext } from './UserContext';
 
 export function UserProvider({ children }) {
   // I define user as state as eventually it will need to be one (instead of const)
-  const [user, setUser] = useState('tickle122');
+  const [user, setUser] = useState({ username: 'tickle122' });
   // {{ user, setUser }} > first brace is to acces js, second brace is to create an object with shorthand
   return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
 }
